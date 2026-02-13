531227b857033358a005c3397c9b246b139806be
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-13 10:31:51 +0700

    Refactor component loading utilities
    Extract overlapping code snipptet to the more generic `load` function
    which will load an arbitrary html component to a specified html tag,
    using the given ID.
    
    Also reformat some code

--- js/dynamic-loader.js
+++ js/dynamic-loader.js
@@ -1,7 +1,11 @@
-function load_tab_bar(current_page) {
-  fetch("/navbar.html")
+async function load(id, html_comp) {
+  return fetch(html_comp)
     .then(res => res.text())
-    .then(html => { document.getElementById("navbar").innerHTML = html; })
+    .then(html => { document.getElementById(id).innerHTML = html; })
+}
+
+function load_tab_bar(current_page) {
+  load("navbar", "/navbar.html")
     .then(() => {
       document.querySelectorAll("a").forEach(link => {
         if (link.getAttribute("href") === current_page) {
@@ -15,10 +19,4 @@ function load_tab_bar(current_page) {
     });
 }
 
-function load_footer() {
-  fetch("/footer.html")
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("footer-placeholder").innerHTML = html;
-    })
-} 
+function load_footer() { load("footer-placeholder", "/footer.html") } 
